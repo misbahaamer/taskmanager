e1876120d5b1451dedd91897bcee221525e0a556
Misbah Aamer <misbahuddin.aamer@gmail.com>
2022-04-01 02:44:09 -0400

    application service service registration

--- taskmanager/Services/Task.API/Startup.cs
+++ taskmanager/Services/Task.API/Startup.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Task.Application;
 
 namespace Task.API
 {
@@ -26,6 +27,8 @@ namespace Task.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationServices();
+
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
---
+++ taskmanager/Services/Task.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Task.Application.Behaviours;
+using AutoMapper;
+using FluentValidation;
+
+namespace Task.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(SystemValidation<,>));
+
+            return services;
+        }
+    }
+}
