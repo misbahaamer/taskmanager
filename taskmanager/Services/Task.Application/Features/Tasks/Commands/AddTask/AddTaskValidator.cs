39f4beb46a0ce2f0076eb2e0b05b863854ca53c5
Misbah Aamer <misbahuddin.aamer@gmail.com>
2022-03-31 23:24:17 -0400

    task validator update

--- taskmanager/Services/Task.Application/Features/Tasks/Commands/AddTask/AddTaskValidator.cs
+++ taskmanager/Services/Task.Application/Features/Tasks/Commands/AddTask/AddTaskValidator.cs
@@ -11,13 +11,13 @@ namespace Task.Application.Features.Tasks.Commands.AddTask
     public class AddTaskValidator : AbstractValidator<AddTaskCommand>
     {
 
-
+        //new task due date cannot be in the past
         public AddTaskValidator()
         {
-            RuleFor(x => x.Name).NotEmpty().WithMessage("{Name} is required").NotNull();
-            RuleFor(x => x.DueDate).NotEmpty().WithMessage("{DueDate} is required").NotNull();
-            RuleFor(x => x.Priority).NotEmpty().WithMessage("{Priority} is required").NotNull();
-            RuleFor(x => x.Status).NotEmpty().WithMessage("{Status} is required").NotNull();
+            RuleFor(x => x.Name).NotEmpty().NotNull().WithMessage("{Name} is required");
+            RuleFor(x => x.DueDate).NotEmpty().NotNull().GreaterThanOrEqualTo(DateTime.Today).WithMessage("{DueDate} is required");
+            RuleFor(x => x.Priority).NotEmpty().NotNull().WithMessage("{Priority} is required");
+            RuleFor(x => x.Status).NotEmpty().NotNull().WithMessage("{Status} is required");
         }
 
        
