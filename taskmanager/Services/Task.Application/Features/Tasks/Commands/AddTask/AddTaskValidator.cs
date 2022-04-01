87e4a92293223ee3615e9b45f71b36b5ac230e48
Misbah Aamer <misbahuddin.aamer@gmail.com>
2022-03-31 23:22:06 -0400

    Task Model Validator

---
+++ taskmanager/Services/Task.Application/Features/Tasks/Commands/AddTask/AddTaskValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task.Application.Contracts.Persistence;
+
+namespace Task.Application.Features.Tasks.Commands.AddTask
+{
+    public class AddTaskValidator : AbstractValidator<AddTaskCommand>
+    {
+
+
+        public AddTaskValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("{Name} is required").NotNull();
+            RuleFor(x => x.DueDate).NotEmpty().WithMessage("{DueDate} is required").NotNull();
+            RuleFor(x => x.Priority).NotEmpty().WithMessage("{Priority} is required").NotNull();
+            RuleFor(x => x.Status).NotEmpty().WithMessage("{Status} is required").NotNull();
+        }
+
+       
+    }
+}
