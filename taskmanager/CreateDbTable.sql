610886ea213a445fda4ce01a011072995aa95b6e
Misbah Aamer <misbahuddin.aamer@gmail.com>
2022-04-01 05:01:01 -0400

    testing add/update/get all

---
+++ taskmanager/CreateDbTable.sql
@@ -0,0 +1,31 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Tasks] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NULL,
+    [Description] nvarchar(max) NULL,
+    [DueDate] datetime2 NOT NULL,
+    [Priority] nvarchar(max) NULL,
+    [Status] nvarchar(max) NULL,
+    [CreatedDate] datetime2 NULL,
+    CONSTRAINT [PK_Tasks] PRIMARY KEY ([Id])
+);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20220401080149_initialcreate', N'5.0.15');
+GO
+
+COMMIT;
+GO
