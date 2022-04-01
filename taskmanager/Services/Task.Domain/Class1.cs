8c738f5698ebdc6a4e8640a69147cd746003d204
Misbah Aamer <misbahuddin.aamer@gmail.com>
2022-03-31 21:36:07 -0400

    domain layer setup

--- taskmanager/Services/Task.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Task.Domain
-{
-    public class Class1
-    {
-    }
-}
---
+++ taskmanager/Services/Task.Domain/Entities/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task.Domain.Common;
+
+namespace Task.Domain.Entities
+{
+    public class Task : EntityBase
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Priority { get; set; }
+        public string Status { get; set; }
+    }
+}
